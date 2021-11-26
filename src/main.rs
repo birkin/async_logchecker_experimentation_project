3568620c3a9eda4722623ea5f373de0beed1606e
birkin <birkin.diana@gmail.com>
2021-11-26 07:33:29 -0500

    implements std-out logging.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+#[macro_use]
+extern crate log;
+
+
 use chrono::Local;
 // use chrono::{DateTime, Local};
 use tokio::time::Instant;
 // use tokio::time::{Duration, Instant};
+
+use env_logger::{Builder, Target};
 
 use logchecker_project::Config;
 
@@ -11,10 +17,17 @@ async fn main() {
     let start_time = Instant::now();
     println!("start_time, ``{:?}``", start_time);
     let local_time = Local::now(); // used for logging
-    println!("local_time, ``{:?}``", local_time);
-    println!("Hello, world!");
+    // println!("local_time, ``{:?}``", local_time);
+    // println!("Hello, world!");
 
     /* load settings */
     let config = Config::new();
     println!("config, ``{:?}``", config);
+
+    /* setup logging */
+    let mut log_builder = Builder::from_default_env();
+    log_builder.target( Target::Stdout );
+    log_builder.init();
+    info!( "{}", format!("\n\n-------\n`starting logchecker_project code at, ``{:?}``", local_time.to_rfc3339()) );
+
 }
